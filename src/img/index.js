7062a38f862e0106a7b4b307685531c1c65226df
boolks <bootre@naver.com>
2020-11-05 15:09:08 +0900

    add DB

--- src/img/index.js
+++ src/img/index.js
@@ -1,2 +1,8 @@
 export const Starbucks = require('./starbucks.png');
 export const Ediya = require('./ediya.png');
+export const Bbaek = require('./bbaek.png');
+export const Hollys = require('./hollys.png');
+export const Twosome = require('./twosome.png');
+export const Tomntoms = require('./tomntoms.png');
+export const Coffeebean = require('./coffeebean.png');
+export const Angelinus = require('./angelinus.png');
