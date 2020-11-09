a2662d64caf52bd0f2a2540a16829fbd4b5b7b18
nick <jobinformation@naver.com>
2020-11-09 10:22:50 +0900

    Straighten out

--- src/components/Select.js
+++ src/components/Select.js
@@ -3,14 +3,13 @@ import Selects from 'react-select';
 
 const options = [
     { value: 1, label: '카페인 함량 오름차순' },
-    { value: 2, label: '카페인 함량 오름차순' },
+    { value: 2, label: '카페인 함량 내림차순' },
     { value: 3, label: '가격 낮은 순' },
     { value: 4, label: '칼로리 낮은 순' } 
   ];
 
 
-  class Select extends React.Component {
-
+class Select extends React.Component {
     state = {
         selectedOption: null,
     }
@@ -18,7 +17,7 @@ const options = [
     this.setState({ selectedOption });
     console.log(`Option selected:`, selectedOption);
     }
-    
+
     render(){
         //const { selectedOption } = this.state;
         
@@ -28,6 +27,6 @@ const options = [
             autoFocus={true} />
         );
     }
-  }
+}
   
   export default Select;
