9f740be835b294d46e2c8c68d9c8ad25f763deeb
boolks <bootre@naver.com>
2020-11-10 09:41:05 +0900

    brandlike

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,22 +25,28 @@ const Home = () => {
     <div className="banner">
       <Slider {...settings}>
         <div className="banner">
-          <img src='https://testbucket20201103-h.s3.amazonaws.com/starbucksnew.png' alt="starbucksnew"/>
+          <img src='https://cacaonewproductbucket-starbucks.s3.amazonaws.com/starbucks_new.PNG' alt="starbucksnew"/>
         </div>
         <div className="banner">
-          <img src='https://testbucket20201103-h.s3.amazonaws.com/starbucksnew.png' alt="starbucksnew"/>
+          <img src='https://cacaonewproductbucket-hollys.s3.amazonaws.com/hollys_new.PNG' alt="hollysnew"/>
         </div>
         <div className="banner">
-          <img src='https://testbucket20201103-h.s3.amazonaws.com/starbucksnew.png' alt="starbucksnew"/>
+          <img src='https://cacaonewproductbucket-tomntoms.s3.amazonaws.com/tomntoms_new.PNG' alt="tomntomsnew"/>
         </div>
         <div className="banner">
-          <img src='https://testbucket20201103-h.s3.amazonaws.com/starbucksnew.png' alt="starbucksnew"/>
+          <img src='https://cacaonewproductbucket-ediya.s3.amazonaws.com/ediya_new.PNG' alt="ediyanew"/>
         </div>
         <div className="banner">
-          <img src='https://testbucket20201103-h.s3.amazonaws.com/starbucksnew.png' alt="starbucksnew"/>
+          <img src='https://cacaonewproductbucket-coffeebean.s3.amazonaws.com/coffeebean_new.png' alt="coffeebeannew"/>
         </div>
         <div className="banner">
-          <img src='https://testbucket20201103-h.s3.amazonaws.com/starbucksnew.png' alt="starbucksnew"/>
+          <img src='https://cacaonewproductbucket-twosome.s3.amazonaws.com/twosome_new.jpg' alt="twosomenew"/>
+        </div>
+        <div className="banner">
+          <img src='https://cacaonewproductbucket-angelinus.s3.amazonaws.com/angelinus_new.PNG' alt="angelinusnew"/>
+        </div>
+        <div className="banner">
+          <img src='https://cacaonewproductbucket-paikdabang.s3.amazonaws.com/paikdabang_new.jpg' alt="paikdabangnew"/>
         </div>
       </Slider>
     </div>
