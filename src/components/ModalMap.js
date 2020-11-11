2cc399b5e8a1eb307ed4d4f01b20a4416ca1e82f
ok2qw66 <ok2qw66@naver.com>
2020-11-11 11:19:14 +0900

    modal map

--- src/components/ModalMap.js
+++ src/components/ModalMap.js
@@ -1,6 +1,19 @@
 /*global kakao */
+import { map } from "lodash";
 import React from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+const brand_map = new Map();
+
+brand_map.set('starbucks','스타벅스');
+brand_map.set("hollys",'할리스');
+brand_map.set("tomntoms",'탐앤탐스');
+brand_map.set("ediya",'이디야');
+brand_map.set("coffeebean",'커피빈');
+brand_map.set("twosome",'투썸플레이스');
+brand_map.set("angelinus",'엔젤리너스');
+brand_map.set("paikdabang",'빽다방');
+
 
 class ModalMap extends React.Component {
   constructor(props) {
@@ -30,33 +43,66 @@ class ModalMap extends React.Component {
         let container = document.getElementById("map");
         let options = {
           center: new kakao.maps.LatLng(37.506502, 127.053617),
-          level: 3
+          level: 5
         };
 
         //지도를 생성합니다.
         let map = new window.kakao.maps.Map(container, options);
-        // 장소 검색 객체를 생성합니다
-        let ps = new kakao.maps.services.Places();
-
-        // 키워드로 장소를 검색합니다
-        ps.keywordSearch('서울 ' + this.props.brand, placesSearchCB);
-
-        // 키워드 검색 완료 시 호출되는 콜백함수 입니다
-        function placesSearchCB(data, status, pagination) {
-          if (status === kakao.maps.services.Status.OK) {
-            // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-            // LatLngBounds 객체에 좌표를 추가합니다
-            var bounds = new kakao.maps.LatLngBounds();
-
-            for (var i = 0; i < data.length; i++) {
-              displayMarker(data[i]);
-              bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-            }
-
-            // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-            map.setBounds(bounds);
-          }
+        
+        // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
+        if (navigator.geolocation) {
+            let brand_eng = this.props.brand;
+            let brand = brand_map.get(brand_eng);
+            //console.log(brand_eng,brand);
+          // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+          navigator.geolocation.getCurrentPosition(function(position) {
+              
+              var lat = position.coords.latitude, // 위도
+                  lon = position.coords.longitude; // 경도
+              
+              var locPosition = new kakao.maps.LatLng(lat, lon) // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+              
+              // 마커와 인포윈도우를 표시합니다
+              //displayMarker(locPosition, message);
+             
+              findPlace(brand,locPosition);
+            
+              // 지도 중심좌표를 접속위치로 변경합니다
+              map.setCenter(locPosition); 
+              
+            });
+          
         }
+
+        function findPlace(brand,locPosition){
+          //console.log('히얼',brand, locPosition);
+          // 장소 검색 객체를 생성합니다
+          let ps = new kakao.maps.services.Places();
+          
+          // 키워드로 장소를 검색합니다
+          ps.keywordSearch(brand, placesSearchCB,{
+            // Map 객체를 지정하지 않았으므로 좌표객체를 생성하여 넘겨준다.
+            location: locPosition});  
+        
+          // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+          function placesSearchCB(data, status, pagination) {
+            if (status === kakao.maps.services.Status.OK) {
+              // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+              // LatLngBounds 객체에 좌표를 추가합니다
+              //console.log(data);
+              var bounds = new kakao.maps.LatLngBounds();
+
+              for (var i = 0; i < data.length; i++) {
+                displayMarker(data[i]);
+                bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+              }
+
+              // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+              //map.setBounds(bounds);
+              }
+            }
+        }
+
 
         // 지도에 마커를 표시하는 함수입니다
         function displayMarker(place) {
@@ -96,7 +142,7 @@ class ModalMap extends React.Component {
           isOpen={this.state.nestedModal}
           toggle={this.toggleNested}
         >
-          <ModalHeader>주변 {this.props.brand} 매장</ModalHeader>
+          <ModalHeader>주변 {brand_map.get(this.props.brand)} 매장</ModalHeader>
           <ModalBody style={{ border: "1px solid orange" }}>
             <div
               id="map"
