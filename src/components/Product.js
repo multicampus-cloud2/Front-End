fc7b1e2b83f909f3d23158ad09052441cf465fac
young <dbsydde@gmail.com>
2020-11-12 14:57:41 +0900

    더보기 구현

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,6 +3,7 @@ import 'css/compare.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faPlus } from "@fortawesome/free-solid-svg-icons";
+import * as common from 'components/common.js';
 
 class Product extends React.Component{
 
@@ -43,7 +44,7 @@ class Product extends React.Component{
                     </div>
                     <div className="product__item__text">
                         <h6><p>{this.props.coffee['name']}</p></h6>
-                        <div className="product__item__price">{this.props.coffee['price']}원</div>
+                        <div className="product__item__price">{common.numberWithCommas(this.props.coffee['price'])}원</div>
                         <div className="cart_add">
                             {/* form 태그를 통해서 자식 컴포넌트에서 부모 컴포넌트로 값을 전달해 줄 수 있음 */}
                             <form onSubmit={this.handleSubmit}>
